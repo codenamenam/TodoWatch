6f98380e505181f4800c1480441b062037ca12c4
bluetire27@gmail.com <bluetire27@gmail.com>
2021-08-24 14:21:52 +0900

    Initial Commit

---
+++ TodoWatch/TodoWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoWatchApp.swift
+//  TodoWatch
+//
+//  Created by nam on 2021/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
