d0102ca570a50209d1990329123674e9f3a85823
codenamenam <bluetire27@gmail.com>
2022-02-04 01:32:14 +0900

    AddTodo modal sheet revised.

--- TodoWatch/HomeScreen/HomeScreenElements/AddTodo.swift
+++ TodoWatch/HomeScreen/HomeScreenElements/AddTodo.swift
@@ -10,36 +10,30 @@ import UIKit
 
 struct AddTodo: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-    @State var name: String = ""
+    @State private var addGroup : String = ""
+    @State private var addTodo : String = ""
     
     var body: some View {
         let screenSize: CGRect = UIScreen.main.bounds
         
         VStack {
+            
             HStack{
-                Text("할 일 추")
-                    .font(.title.bold())
-                    .padding()
-                
+                Spacer()
                 Button(action: {
                     self.presentationMode.wrappedValue.dismiss()
                 }) {
-
-                    let text: Text = Text("+")
-                    text.font(.title.bold())
-                        .rotationEffect(Angle(degrees: 41))
-                        .background(Circle().fill(Color.gray))
-                        .foregroundColor(.white)
-                        /*.font(.title.bold())
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .rotationEffect(Angle(degrees: 45))*/
-                    
-                        
-                    
+                    Image("Graphics_X_Button")
+                        .resizable()
+                        .aspectRatio(CGSize(width: 0.1, height: 0.1), contentMode: .fit)
+                        .frame(width: 50, height: 50)
                 }
-                .padding()
+                .padding(.trailing, 30)
             }
+            
+            Text("할 일 추가")
+                .font(.title.bold())
+                .multilineTextAlignment(.center)
             
             Path{ path in
                 path.move(to: CGPoint(x: screenSize.origin.x + 40, y: 0))
@@ -47,26 +41,62 @@ struct AddTodo: View {
             }
             .stroke(lineWidth: 1.0)
             .frame(width: screenSize.width, height: 1, alignment: .center)
-            .foregroundColor(Color(Color.RGBColorSpace.sRGB, red: 0/255, green: 0/255, blue: 0/255, opacity: 0.2))
+            .foregroundColor(.lightGray)
             
             HStack{
-                Spacer()
+                Spacer(minLength: 40)
                 Text("그룹")
                     .font(.title2.bold())
-                    .padding()
                 
-                TextField("입력하세요", text: $name){
-                }
-                .padding()
-                .textContentType(.creditCardNumber)
-                    
-                    
+                
+                TextField("입력하세요", text: $addGroup)
+                    .padding()
+                    .underlineTextField()
                 Spacer()
             }
             
+            HStack{
+                Text("할 일")
+                    .font(.title2.bold())
+                    .padding(.leading, 40)
+                Spacer()
+            }
+            
+            ZStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.lightGray)
+                    .frame(width: 330, height: 60, alignment: .center)
+                
+                
+                
+                TextField("안녕?", text : $addTodo)
+                    .frame(width: 300, height: 60, alignment: .center)
+                
+            }
+            
         }
+        .frame(height: 500)
+    }
+        
+}
+
+extension Color {
+    static let lightGray = Color(RGBColorSpace.displayP3, red: 0, green: 0, blue: 0, opacity: 0.2)
+}
+
+
+extension View {
+    func underlineTextField() -> some View {
+        self
+            .padding(.vertical, 10)
+            .overlay(Rectangle().frame(height: 2).padding(.top, 35).padding(.trailing).padding(.leading))
+            .foregroundColor(.lightGray)
+            .padding(10)
     }
 }
+
+
+
 
 struct AddTodo_Previews: PreviewProvider {
     static var previews: some View {
